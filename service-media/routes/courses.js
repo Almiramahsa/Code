89f90388884a034261785f3ba726c66edd3e3b14
Almira Mahsa <almiramahsa9@gmail.com>
2023-02-10 12:35:15 +0700

    routers set up

---
+++ service-media/routes/courses.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const { APP_NAME } = process.env;
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.send(APP_NAME);
+});
+
+module.exports = router;
