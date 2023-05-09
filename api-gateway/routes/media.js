38ace25e47dc0774411adde3c6cd0df66b7a4e93
Almira Mahsa <almiramahsa9@gmail.com>
2023-05-09 22:59:19 +0700

    integrasi ENDPOINT  GET  api-gateway ke service-media

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const mediaHandler = require('./handler/media');
 
 router.post('/', mediaHandler.create);
+router.get('/', mediaHandler.getAll);
 module.exports = router;
